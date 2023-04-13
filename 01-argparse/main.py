ddc9ff4121eee76d272e208b42f55bb818eafb82
Moenen Erbuer <moenen@arthur.io>
2023-04-13 18:26:15 +0300

    -

--- 01-argparse/main.py
+++ 01-argparse/main.py
@@ -1,6 +1,7 @@
+
 import argparse
 
-parser = argparse.ArgumentParser(prog='calc', description='This is a test')
+parser = argparse.ArgumentParser(description='This is a test')
 parser.add_argument('integers', metavar='N', type=int, nargs='+',
                     help='an integer for the accumulator')
 parser.add_argument('--sum', dest='accumulate', action='store_const',
@@ -8,16 +9,17 @@ parser.add_argument('--sum', dest='accumulate', action='store_const',
                     help='sum the integers (default: find the max)')
 
 args = parser.parse_args()
-print(args.accumulate(args.integers))
 
-
-print("""
+if args:
+    print(args.accumulate(args.integers))
+else:
+    print("""
 Try the following commands:
 ---------------------------
 
-calc 1 3 5
+python3 main.py 1 3 5
 
-calc 10 20 40 5 --sum
+python3 main.py 10 20 40 5 --sum
 
-calc -h
+python3 main.py -h
 """)
