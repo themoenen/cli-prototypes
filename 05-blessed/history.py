ce84ec0cdb73ec5b89e31c71fb26cf46f8be45c4
Moenen Erbuer <moenen@arthur.io>
2023-04-19 18:33:06 +0300

    gitignore

--- 05-blessed/history.py
+++
@@ -1,9 +0,0 @@
-
-# key_actions = {
-#     term.KEY_UP: lambda: max(cursor_pos - 1, 0),
-#     term.KEY_DOWN: lambda: min(cursor_pos + 1, len(json_content)),
-#     term.KEY_ENTER: lambda: None if isinstance(json_content[cursor_pos], dict) else -1,
-#     term.KEY_ESCAPE: lambda: -1
-# }
-# for k, v in key_actions.items():
-#     term.on(k)(lambda v=v: setattr(cursor_pos, "value", v()))
